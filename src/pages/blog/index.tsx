ac3990e8709ccdb14d24577120452ebefbced200
Leonardo Perassoli <leonardoperass@gmail.com>
2026-04-30 15:17:30 -0300

    feat: add header component and blog page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/header";
+
+export default function BlogPage () {
+    return (
+        <>
+        <Header />
+        <h2>
+            Blog
+        </h2>
+        </>
+    )
+}
