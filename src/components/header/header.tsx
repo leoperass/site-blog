4a997e77cacd3177e3cffce5b7848ae0bd127050
Leonardo Perassoli <leonardoperass@gmail.com>
2026-04-30 19:43:55 -0300

    feat: add active-link component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import { Button } from "../ui/button"
 import { useRouter } from "next/router"
 import { cn } from "@/lib/utils";
+import { ActiveLink } from "../active-link";
 
 
 export const Header = () => {
@@ -16,9 +17,11 @@ export const Header = () => {
                     <Link href="/">Logo</Link>
 
                     <nav className="flex items-center gap-6">
-                        <Link href="/" className={cn('text-sm font-medium transition-colors hover:text-blue-500', isHomePage ? 'text-blue-500' : 'text-muted-foreground')}>Home</Link>
-                        <Link href="/blog" className={cn('text-sm font-medium transition-colors hover:text-blue-500', isBlogPage ? 'text-blue-500' : 'text-muted-foreground')}>Blog</Link>
-                        <Button variant="secondary">Começar</Button>
+                        <ActiveLink href="/">Início</ActiveLink>
+                        <ActiveLink href="/blog">Blog</ActiveLink>
+                        <Button variant="secondary" asChild>
+                            <Link href="/comecar">Começar</Link>
+                        </Button>
                     </nav>
                 </div>
             </div>
