ad7f3f820685d5dabaa3e4a77eb4750efa5275e8
Leonardo Perassoli <leonardoperass@gmail.com>
2026-05-12 15:14:59 -0300

    feat: configure Tailwind with project style guide and update layout styles

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,3 +1,4 @@
+import { Inter } from "next/font/google";
 import { Footer } from "../footer/footer";
 import { Header } from "../header"
 
@@ -5,9 +6,11 @@ type LayoutProps = {
     children: React.ReactNode;
 }
 
+const inter = Inter({ subsets: ['latin'] });
+
 export const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className="relative flex flex-col min-h-screen bg-black/70">
+        <div className={`${inter.className} relative flex flex-col min-h-screen bg-gray-800`}>
             <Header />
             <main className="flex-1 flex flex-col mb-12">
                 {children}
