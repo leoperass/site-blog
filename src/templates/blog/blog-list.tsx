947ec34c0fe6e2179dd740e85f14e2893420de54
Leonardo Perassoli <leonardoperass@gmail.com>
2026-06-23 16:37:16 -0300

    feat: implement post card component and blog list integration

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,5 +1,6 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
+import { PostCard } from "./components/post-card";
 
 export function BlogList() {
     const router = useRouter();
@@ -27,6 +28,18 @@ export function BlogList() {
             </header>
 
             {/* {LISTAGEM DE POSTS} */}
+            <PostCard
+                slug="/transformando" 
+                title="Transformando seu negócio em uma loja virtual"
+                date="20/12/2024"
+                description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+                image="/assets/first-post.png"
+                author={{
+                    avatar: '/avatar-01.svg',
+                    name: 'Aspen Dokids'
+                }}
+
+            />
         </div>
     )
 }
