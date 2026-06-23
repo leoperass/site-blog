0e5642628234dc9cae3a3429881119bdbc767ba6
Leonardo Perassoli <leonardoperass@gmail.com>
2026-06-23 13:44:27 -0300

    style: add backdrop blur effect to header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -7,7 +7,7 @@ import { Logo } from "../../logo";
 export const Header = () => {
 
     return (
-        <header className="fixed top-0 z-50 w-full border-b border-white/10">
+        <header className="fixed top-0 z-50 w-full border-b border-white/10 backdrop-blur-md">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                     <Logo />
