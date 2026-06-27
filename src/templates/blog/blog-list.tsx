1b3fd3eff5469ea445cf7047a4bb7177562cb0cf
Leonardo Perassoli <leonardoperass@gmail.com>
2026-06-27 19:48:44 -0300

    feat: add initial blog post and dynamic post rendering

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,17 +1,21 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components/post-grid-card";
+import { allPosts } from "contentlayer/generated";
 
 export function BlogList() {
     const router = useRouter();
     const query = router.query.q as string;
-    const pageTitle = query 
+    const pageTitle = query
         ? `Resultados de busca para "${query}"`
         : 'Dicas e estratégias para impulsionar seu negócio';
 
+    const posts = allPosts;
+
     return (
         <div className="flex flex-col py-24 flex-grow h-full">
-            <header className="">
+            <header className="pb-14">
                 <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
                     <div className="flex flex-col gap-4 md:px-0">
                         {/* {TAG} */}
@@ -28,18 +32,22 @@ export function BlogList() {
             </header>
 
             {/* {LISTAGEM DE POSTS} */}
-            <PostCard
-                slug="/transformando" 
-                title="Transformando seu negócio em uma loja virtual"
-                date="20/12/2024"
-                description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
-                image="/assets/first-post.png"
-                author={{
-                    avatar: '/avatar-01.svg',
-                    name: 'Aspen Dokids'
-                }}
-
-            />
+            <PostGridCard>
+                {posts.map((post) => (
+                    <PostCard
+                        key={post._id}
+                        slug={post.slug}
+                        title={post.title}
+                        date={new Date(post.date).toLocaleDateString("pt-BR")}
+                        description={post.description}
+                        image={post.image}
+                        author={{
+                            avatar: post.author?.avatar,
+                            name: post.author?.name
+                        }}
+                    />
+                ))}
+            </PostGridCard>
         </div>
     )
 }
