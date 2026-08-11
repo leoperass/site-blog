aec8e198372a243d33d0543b8de4430b3a28693b
Leonardo Perassoli <leonardoperass@gmail.com>
2026-08-11 11:20:54 -0300

    refactor: migrate blog post generation and revalidation to App Router

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,6 +8,14 @@ type BlogPostPageProps = {
   }>
 }
 
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug
+  }));
+}
+
 export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
