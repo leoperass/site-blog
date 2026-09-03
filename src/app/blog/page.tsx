f8fd3b48f9e9832a1637daae49305b5b142c489d
Leonardo Perassoli <leonardoperass@gmail.com>
2026-09-03 14:57:52 -0300

    chore: add seo in blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    url: 'https://site-blog-sepia-alpha.vercel.app/',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-blog-sepia-alpha.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set'
+      }
+    ]
+  }
+}
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort((a,b) => new Date(b.date).getTime() - new Date(a.date).getTime());
