a2e17825d1fb48c1e40624c996d14b20dc519361
Leonardo Perassoli <leonardoperass@gmail.com>
2026-09-03 19:44:04 -0300

    chore: add seo in blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
-import { PostPage } from "@/templates/blog";
-import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { allPosts } from "contentlayer/generated";
+import { PostPage } from "@/templates/blog";
 
 type BlogPostPageProps = {
   params: Promise<{
@@ -9,6 +10,25 @@ type BlogPostPageProps = {
 }
 
 export const revalidate = 60;
+
+export async function generateMetadata({ params }: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {}
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image]
+    }
+  }
+}
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -25,6 +45,6 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
   }
 
   return (
-    <PostPage post={post}/>
+    <PostPage post={post} />
   )
 }
